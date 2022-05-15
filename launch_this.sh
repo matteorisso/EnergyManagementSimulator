40836487dc3f85db66703b5c86cf6f02c112829a
s282133 <s282133@studenti.polito.it>
2022-05-15 10:52:28 +0200

    added script to launch sim

--- launch_this.sh
+++ launch_this.sh
@@ -4,16 +4,25 @@ cd simulator
 
 cd src
 
+# delete sensors
+ls | grep -P "sensor[0-9]*" | xargs -d"\n" rm
+ls | grep -P "converter[0-9]+" | xargs -d"\n" rm -r
+
+# launch python script that creates the system according to settings.json
 python3 ./readpattern.py
 
 cd ..
 
 cd work
 
+# build the system
 make
 
+# launch the simulation
 ./run.x
 
+# create matlab script to plot the results
 python3 ./read_trace.py
 
-#sudo cp ./traces.m /home/anto/Desktop/shared
+# tuned for my system !
+sudo cp ./traces.m /home/anto/Desktop/shared
