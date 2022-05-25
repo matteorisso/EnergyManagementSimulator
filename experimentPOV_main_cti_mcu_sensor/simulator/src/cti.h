bae2e0bab15e7f5b64d395bbac6c7c0798e20c86
s282133 <s282133@studenti.polito.it>
2022-05-25 12:46:54 +0200

    system not schedulable

--- experimentPOV_main_cti_mcu_sensor/simulator/src/cti.h
+++ experimentPOV_main_cti_mcu_sensor/simulator/src/cti.h
@@ -6,13 +6,14 @@ SCA_TDF_MODULE (cti)
 
 double retrievedData;
 
-sca_tdf::sca_in<int> CTItoMCU_enS1, CTItoS1_done;
-sca_tdf::sca_in<double> CTItoS1_data;
-sca_tdf::sca_out<double>  CTItoMCU_dataS1;
-sca_tdf::sca_out<int>  CTItoMCU_doneS1, CTItoS1_en;
+sca_tdf::sca_in<int> enableSensor1_MCU, doneSensor1_S1;
+sca_tdf::sca_in<double> dataSensor1_S1;
+sca_tdf::sca_out<double>  dataSensor1_MCU;
+sca_tdf::sca_out<int>  doneSensor1_MCU, enableSensor1_S1;
 
-SCA_CTOR(cti): doneSensor1_S1("CTItoS1_done"), enableSensor1_S1("CTItoS1_en"), dataSensor1_S1("CTItoS1_data"), doneSensor1_MCU("CTItoMCU_doneS1"), \
-enableSensor1_MCU("CTItoMCU_enS1"), dataSensor1_MCU("CTItoMCU_dataS1"){};
+SCA_CTOR(cti): doneSensor1_S1("doneSensor1_S1"), enableSensor1_S1("enableSensor1_S1"), \
+dataSensor1_S1("dataSensor1_S1"), doneSensor1_MCU("doneSensor1_MCU"), \
+enableSensor1_MCU("enableSensor1_MCU"), dataSensor1_MCU("dataSensor1_MCU"){};
 
 
 void set_attributes();
